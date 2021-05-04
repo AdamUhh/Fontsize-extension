93a233e92e8fa741406ef24de086f9c6dd0b630b
AdamUhh <adam.okay@yahoo.com>
2021-05-04 20:34:25 +0400

    added feature where user can choose what element to change the font size of

--- content_script.js
+++ content_script.js
@@ -1,14 +1,17 @@
-let style = document.createElement('style');
+let style;
+style = document.createElement('style');
 document.body.appendChild(style);
 
 browser.storage.onChanged.addListener((changes, area) => {
-    if (area === 'local' && 'value' in changes) {
-        update(changes.value.newValue);
+    if (area === 'local' && 'value' in changes && 'elements' in changes && 'checked' in changes) {
+        if (changes.checked.newValue == 1) {
+            update(changes.value.newValue, changes.elements.newValue);
+        } else {
+            style.innerText = '';
+        }
     }
 });
 
-function update(value) {
-    style.innerText = `p { font-size: ${value}px} !important`;
+function update(value, elements) {
+    style.innerText = `${elements} { font-size: ${value}px} !important`;
 }
-
-browser.storage.local.get('value').then((result) => update(result.value));
--- popup.js
+++ popup.js
@@ -1,27 +1,46 @@
 const range = document.getElementById('range'),
-    rangeV = document.getElementById('rangeV')
+    rangeV = document.getElementById('rangeV');
+let selElement = document.getElementById('selElement');
+let switcher = document.getElementById('switch');
 
-    range.addEventListener('change', (e) => {
-    setValue(e.target.value);
+selElement.addEventListener('change', (e) => {
+    setValue({ value: range.value, elements: selElement.value, checked: switcher.checked });
+});
+
+switcher.addEventListener('change', (e) => {
+    console.log(selElement.value);
+    setValue({ value: range.value, elements: selElement.value, checked: switcher.checked });
+});
+
+range.addEventListener('change', (e) => {
+    setValue({ value: e.target.value, elements: selElement.value, checked: switcher.checked });
 });
 
 range.addEventListener('dblclick', (e) => {
     range.value = 20;
-    setValue(20);
+    setValue({ value: 20, elements: selElement.value, checked: switcher.checked });
     setValueRange();
 });
 
-async function setValue(value) {
-    await browser.storage.local.set({ value });
+async function setValue(dict) {
+    if (dict.checked == 1 || dict.checked) await browser.storage.local.set({ value: dict.value, elements: dict.elements, checked: 1 });
+    else await browser.storage.local.set({ value: dict.value, elements: dict.elements, checked: 0 });
 }
 
 async function init() {
     let { value } = await browser.storage.local.get('value');
+    let { elements } = await browser.storage.local.get('elements');
+    let { checked } = await browser.storage.local.get('checked');
 
+    if (checked == undefined) checked = 1;
+    if (!elements) elements = 'p';
     if (!value) value = 20;
 
     range.value = value;
-    setValue(value);
+    selElement.value = elements;
+    switcher.checked = checked == 1;
+
+    setValue({ value: value, elements: elements, checked: switcher.checked });
     setValueRange();
 }
 
